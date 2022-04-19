419fc0a79945136ee463a547f79fbf2925b73ec1
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-04-19 12:20:01 +0200

    exo3

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const testDom = document.querySelectorAll('.important')
+let allImg = document.querySelectorAll('img')
 
 for (let index = 0; index < testDom.length; index++) {
     const element = testDom[index];
     element.setAttribute("title", "This is an important item");
-    console.log(testDom)
+ console.log(testDom)
     
 }
 
+
+for (let index = 0; index < allImg.length; index++) {
+    const element = allImg[index];
+    
+    if(element.className !== 'important' ) {
+        element.style.display = 'none';
+    }
+
+    console.log("ImgImportant")
+}
 
 
 
