60355e82066c8129d9a2c6b8a40d4d6a6742f860
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-04-19 16:57:05 +0200

    pt4

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
-let allText = document.querySelectorAll('p');
 
 
-function getRandomRgb() {
-    let num = Math.round(0xffffff * Math.random());
-    let r = num >> 16;
-    let g = num >> 8 & 255;
-    let b = num & 255;
-    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
-  }
-  
-  for (let i = 0; i < 10; i++) {
-    console.log(getRandomRgb());
-  }
-
-
-
-for (let index = 0; index < allText.length; index++) {
-    const element = allText[index];
-    if(element.className !== 'important') {
-
-        element.style.color = getRandomRgb();
+function randomColors() {
+    
+    let letters = '0123456789ABCDEF'
+    let color = '#'
+    
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
-
-
+    return color;
     
 }
 
+console.log(randomColors())
+
+let arr = ["Gilles", "Jamie", "Jason", "Jeremy", "Martin", "Emre", "Rayane", "Rayhan", "Ricardo", "Steffanie", "Sebastien", "Tanguy", "Thibaud", "Zakaria"];
+
+
+for (let elem of arr) {
+    let newSection = document.createElement('section')
+    let newText = document.createTextNode(elem)
+    let selectArt = document.querySelector('article')
+    newSection.appendChild(newText);
+    selectArt.appendChild(newSection);
+    newSection.style.backgroundColor = randomColors();
+    
+    console.log("Here's " + elem + "!");
+    
+    
+}
+
+
+// function addSection() {
+
+//     var tag = document.createElement("p"); // <p></p>
+//     var text = document.createTextNode("TEST TEXT"); 
+//     tag.appendChild(text); // <p>TEST TEXT</p>
+//     var element = document.getElementsByTagName("body")[0];
+//     element.appendChild(tag); // <body> <p>TEST TEXT</p> </body>
+//     return
+// }
 
 
 
