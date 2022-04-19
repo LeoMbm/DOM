630b664e8f8279b9a8cc9c28cb2666a9b9db4059
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-04-19 11:46:06 +0200

    exo2

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-document.title = "Modifying the DOM"
+const testDom = document.querySelectorAll('.important')
 
-function getRandomRgb() {
-    var num = Math.round(0xffffff * Math.random());
-    var r = num >> 16;
-    var g = num >> 8 & 255;
-    var b = num & 255;
-    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
-  }
-  
-  for (var i = 0; i < 10; i++) {
-    console.log(getRandomRgb());
-  }
-
-  document.body.style.backgroundColor = getRandomRgb() ;
-
-  const myElement = document.getElementById('content');
-
-for (let i = 0; i < myElement.children.length; i++) {
-  console.log(myElement.children[i]);
+for (let index = 0; index < testDom.length; index++) {
+    const element = testDom[index];
+    element.setAttribute("title", "This is an important item");
+    console.log(testDom)
+    
 }
+
+
+
+
