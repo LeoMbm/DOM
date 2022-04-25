7044a278fc41d46fbde818018d225bd3c645c73b
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-04-25 15:34:37 +0200

    form exo 2 ok

--- form.js
+++ form.js
@@ -3,6 +3,7 @@ const spanName = document.getElementById('display-firstname')
 const age = document.getElementById('age')
 const pwd = document.getElementById('pwd')
 const dark = document.getElementById('toggle-darkmode')
+const hard = document.getElementById('a-hard-truth')
 
 
 
@@ -11,7 +12,26 @@ surName.addEventListener('keyup', (e) => {
     spanName.innerText = surName.value
     surName.appendChild(spanName)
     
-    // displayName(spanName);
+   
     
 })
 
+
+age.addEventListener('keyup', (e) => {
+
+  check(age.value);
+})
+
+function check(){
+
+    if (age.value >= 18){
+
+        hard.style.visibility = 'visible';
+
+    }
+    else if (age.value < 18){
+
+        hard.style.visibility = 'hidden';
+    }
+    
+}
